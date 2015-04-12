68f2dbe46ca7c58832a3fa3ef8818d9ec3b62c14
Eden Mazzola <mazzolaeden@gmail.com>
2015-04-11 17:36:08 -0700

    Finihsed binarary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,33 +1,65 @@
-var extend = function(to, from){
-  for(var k in from){
-    to[k] = from[k];
-  }
-};
-
 
 var BinarySearchTree = function(value){
 	var binarySearchTree = {};
 
 	binarySearchTree.value = value;
+	binarySearchTree.left = null;
+	binarySearchTree.right = null;
 
-	extend(binarySearchTree, binarySearchTreeMethods);
+	binarySearchTree.insert = function(value){
+		if(value > this.value){
+		//if value is larger than this.value then check property right.
+			if(this.right === null){
+				this.right = BinarySearchTree(value);
+				return;
+			}
+				this.right.insert(value);
+		} else {
+		//if value is less than this.value then check property left.
+			if(this.left === null){
+				this.left = BinarySearchTree(value);
+				return;
+			}
+			this.left.insert(value);
+		}
+	};
 
+	binarySearchTree.contains = function(value){
+
+		if(this.value === value) {
+			return true;
+		}
+		if(this.value > value) {
+			if(this.left === null){
+				return false;
+			}
+			return this.left.contains(value);
+		} else {
+			if(this.right === null){
+				return false;
+			}
+			return this.right.contains(value);
+		}
+		
+	};
+	
+	binarySearchTree.depthFirstLog = function(func){
+		func(this.value);
+
+		if(this.left){
+			this.left.depthFirstLog(func);
+		}
+		if(this.right){
+			this.right.depthFirstLog(func);
+		} 
+	};
 	return binarySearchTree;
 };
 
 
 
- var binarySearchTreeMethods = {};
 
 
-binarySearchTreeMethods.insert = function(value){
-};
-
-binarySearchTreeMethods.contains = function(){
-};
-
-binarySearchTreeMethods.depthFirstlog = function(){
-};
 
 
 
